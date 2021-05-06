b56afb1b7aa8f50e2ce3a54080e136589eb8ed2c
Ruslan Zhedunov <rzhedunov@mail.ru>
2021-05-06 13:51:06 +0400

    Заголовок, центровка

--- gui3.py
+++ gui3.py
@@ -72,6 +72,15 @@ def main():
     knopkaSave.pack()
     knopkaExit.pack()
     
+    root.title('Простой GUI')
+    w = 800
+    h = 650
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    
     root.mainloop ()
     
 if __name__ =='__main__':
